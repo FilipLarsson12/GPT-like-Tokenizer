b5aaf6124b4f63bbfb8b9fd62dfa042af865b513
Filip Larsson <filiplarssonnnn@gmail.com>
2024-06-09 15:25:45 +0200

    t

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from tokenizer import Tokenizer
 def main():
 
     my_tokenizer = Tokenizer()
-    pairs = my_tokenizer.train("bccccaaa", 258)
-    
+    pairs = my_tokenizer.train("aaaaaaaaaabbbbbbbcccc", 260)
+
     # HALLOOOOOOOOOOOOO
 
 # Ensure the main function is called when the script is executed
--- tokenizer.py
+++ tokenizer.py
@@ -10,7 +10,7 @@ class Tokenizer:
         return tokens
 
     # Find all the character-pairs in a text and return them in a dict
-    def find_pairs(self, text):
+    def count_pairs(self, text):
         counts = {}
         for ch1, ch2 in zip(text, text[1:]):
             if (ch1, ch2) in counts:
@@ -22,15 +22,15 @@ class Tokenizer:
         counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
         return counts
     
-    def merge(text, pair, new_id):
+    def merge(self, text, pair, new_id):
         i = 0
         while i < (len(text)-1):
             current_pair = (text[i], text[i+1])
-            print(current_pair)
             if current_pair == pair:
                 text.pop(i)
                 text.pop(i)
                 text.insert(i, new_id)
+                self.merges[pair] = new_id
             i += 1
         return text
 
@@ -38,11 +38,17 @@ class Tokenizer:
     def train(self, text, vocab_size, verbose=False):
         text = self.encode_text(text)
 
-        self.vocab = {idx: bytes([idx]) for idx in range(256)}
-
         add_elms = vocab_size - 256
-        pair_counts = self.find_pairs(text)
-        print(pair_counts)
-        pairs_to_add = list(pair_counts.items())[:add_elms]
-        
 
+        print("Text before merges: ", text)
+        for i in range(add_elms):
+            pair_counts = self.count_pairs(text)
+            print(pair_counts)
+            pair_to_replace = list(pair_counts.items())[0][0]
+
+            print(f"Replacing all occurences of: {pair_to_replace} with {256+i}")
+
+            text = self.merge(text, pair_to_replace, 256+i)
+
+        print("Text after merges: ", text)
+        print("Merges: ", self.merges)
